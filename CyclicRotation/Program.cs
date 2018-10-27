465ef85155c5b1872a32714af8b1d98b1dc98bf2
Dmytro <33005978+Dmytro-Kuzminskyi@users.noreply.github.com>
2018-10-27 20:31:17 +0200

    first commit

---
+++ CyclicRotation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CyclicRotation
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Solution s = new Solution();
+			Console.Write("Enter N: ");
+			int N = int.Parse(Console.ReadLine());
+			Console.Write("Enter K: ");
+			int K = int.Parse(Console.ReadLine());
+			int[] A = new int[N];
+			for (int i = 0; i < N; i++)
+			{
+				Console.Write("Enter {0} element: ", i + 1);
+				A[i] = int.Parse(Console.ReadLine());
+			}
+			Console.Write("The result is [{0}]", string.Join(" ",s.solution(A, K)));
+			Console.ReadKey();
+		}
+	}
+}
---
+++ CyclicRotation/Solution.cs
@@ -0,0 +1,25 @@
+﻿namespace CyclicRotation
+{
+	class Solution
+	{
+		public int[] solution(int[] A, int K)
+		{
+			if (A.Length > 0)
+			{
+				int lastPosition;
+				while (K > 0)
+				{
+					lastPosition = A[A.Length - 1];
+					for(int i = A.Length - 2; i >= 0; i--)
+					{
+						A[i + 1] = A[i];
+					}
+					A[0] = lastPosition;
+					K--;
+				}
+				return A;
+			}
+			else return A;
+		}
+	}
+}
